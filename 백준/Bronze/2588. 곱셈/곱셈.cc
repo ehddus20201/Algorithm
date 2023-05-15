8763f41be279587590fd355d4ad242bb332a3c60
nohdongyeon <ehddus20201@naver.com>
2023-05-15 11:53:43 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main() {
+	int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d\n",b%10*a);
+    printf("%d\n",b%100/10*a);
+    printf("%d\n",b/100*a);
+    printf("%d",b%10*a+b%100/10*a*10+b/100*a*100);
+	return 0;
+}
