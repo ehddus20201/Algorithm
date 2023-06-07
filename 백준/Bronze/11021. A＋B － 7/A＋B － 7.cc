2e8b0b5354b1000c54ca3680bb83112bf494ffd9
nohdongyeon <ehddus20201@naver.com>
2023-06-07 10:54:16 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int n,a,b;
+    scanf("%d",&n);
+    int c=1;
+    for(n;n>=1;n--)
+    {
+        scanf("%d %d",&a,&b);
+        printf("Case #%d: %d\n",c,a+b);
+        c+=1;
+    }
+    
+    return 0;
+}
