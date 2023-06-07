4cbc9a94ca7770c72a4356360305622b6ad56f60
nohdongyeon <ehddus20201@naver.com>
2023-06-07 10:55:04 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 348 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,n;
+    scanf("%d",&n);
+    for(n;n>=1;n--)
+    {
+        scanf("%d %d",&a,&b);
+        printf("%d\n",a+b);
+        
+        
+    }
+
+
+
+
+
+    return 0;
+}
