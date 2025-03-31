2b3f69a680d5c4984ccab4d381de26977483fc95
nohdongyeon <ehddus20201@naver.com>
2025-03-31 20:35:59 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,11 +1,10 @@
 function solution(num_list) {
     let new_list = [];
-    let cnt = 0;
-    let listlen = num_list.length;
-    while(num_list.length>cnt){
-        new_list.push(num_list[listlen-1])
-        cnt = cnt + 1;
-        listlen = listlen - 1;
+    let cnt = num_list.length - 1;
+    while(cnt >= 0){
+        new_list.push(num_list[cnt])
+        cnt = cnt - 1;
+        
     }
         return new_list;
 }
