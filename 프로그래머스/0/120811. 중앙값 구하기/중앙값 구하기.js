b52a7190112834735aa68e6505dd089f00070219
nohdongyeon <ehddus20201@naver.com>
2025-04-21 13:31:29 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,24 +1,6 @@
 function solution(array) {
-    let newArray = [];
     
-    for(let arraycnt = 0; arraycnt<array.length; arraycnt++){
-        let minValue = 1000;
+    return array.sort((a,b)=>a-b)[Math.floor(array.length/2)];
     
-    for(let cnt = 0; cnt<array.length; cnt++){
-        if(minValue>array[cnt]){
-            minValue=array[cnt];
-        }
-        
-    }
-    for(let cnt2 = 0; cnt2<array.length; cnt2++){
-        if(minValue===array[cnt2]){
-            array[cnt2]=1000;
-            break;
-        }
-        
-    }
-    newArray.push(minValue);
-        
-    }
-    return newArray[Math.floor(newArray.length/2)];
+
 }
