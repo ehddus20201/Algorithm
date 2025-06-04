36786969f79586fe256e19f2cfd81da761f015ae
nohdongyeon <ehddus20201@naver.com>
2025-06-04 23:19:55 +0900

    [level 1] Title: 직사각형 별찍기, Time: 74.24 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,15 +2,13 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    let star = "";
-    for(let i=0; i<b;i++){
-        for(let j=0; j<a;j++){
-            star = star + "*";
-            
-        }
-        star= star+"\n";
+    let star='';
+    for(let j = 0 ; j < b; j++){ 
         
+        for(let i = 0 ; i < a; i++){
+            star+='*';
+        }
+        console.log(star);
+        star = '';
     }
-    console.log(star);
-    
 });
