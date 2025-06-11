db49d9ff3a35495b4f4b676a3ec959715268c324
nohdongyeon <ehddus20201@naver.com>
2025-06-11 22:59:26 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 24.28 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,15 +1,34 @@
 function solution(arr)
 {
+//    const answer = [];
+//     for(let i = 0; i < arr.length; i++){    
+//         if(arr[i]===arr[i-1]){
+//             continue;
+//         }
+//         answer.push(arr[i]);       
+//     }
+//    return answer;
     
-    let answer = [];
-    for(let i=0; i<arr.length;i++){
-        if(arr[i]===arr[i+1]){
-            continue;
+    
+    
+//     const answer = [];
+//     let temp = -1;
+//     arr.forEach((item)=>{
+//         if(temp!==item){
+//             answer.push(item);
+//         }
+//         temp = item;      
+//     })
+//    return answer;
+    
+    
+    return  arr.reduce((acc, cur) => {
+        if (acc[acc.length - 1] !== cur) { // acc[-1]= undefined;
+            acc.push(cur);
         }
-        else{
-            answer.push(arr[i]);
-        }
-        
-    }
-    return answer;
+            return acc;
+        }, []); //reduce의 초기값을 설정하지 않으면, arr[0] 이 초기값이 됩니다. 그렇기 때문에 숫자형이 초기값이 되고 push를 수행할 수 없기떄문에 오류 발생
+
+    
+    
 }
